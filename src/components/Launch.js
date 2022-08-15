cde37f253f8c65dd603feba1e09df7a642089492
Amy Chen <amysbubble@Amys-MacBook-Pro-2.local>
2022-08-15 11:41:28 -0700

    bug fix

--- src/components/Launch.js
+++ src/components/Launch.js
@@ -6,7 +6,6 @@ import ErrorComponent from "./ErrorComponent";
 import { fetchEnvData, getEnv, queryPatientIdKey } from "../util/util.js";
 import "../style/App.scss";
 
-
 const fetchContextJson = async (authURL) => {
   if (!authURL) {
     // default, if no auth url provided
@@ -18,10 +17,23 @@ const fetchContextJson = async (authURL) => {
   const response = await fetch(authURL, {
     // include cookies in request
     credentials: "include",
-  }).catch((e) => console.log("Error retrieving context json via auth url ", e));
+  }).catch((e) => {
+    console.log(e);
+    throw new Error("Error retrieving context json via auth url. See console for detail.");
+  });
+
+  if (!response.ok) {
+    console.log(response.status, response.statusText);
+    throw new Error(response.status.toString());
+  }
+
   const contextJson = await response
     .json()
-    .catch((e) => console.log("Context json parsing error ", e));
+    .catch((e) => {
+      console.log(e);
+      throw new Error("Context json parsing error. See console for detail.");
+    });
+
   return contextJson;
 };
 
@@ -33,32 +45,39 @@ export default function Launch() {
     const backendURL = getEnv("REACT_APP_BACKEND_URL");
     const authURL = backendURL
       ? `${backendURL}/auth/auth-info`
-      : "launch-context.json";
+      : "";
     const urlParams = new URLSearchParams(window.location.search);
     //retrieve patient id from URL querystring if any
     const patientId = urlParams.get("patient");
     console.log("patient id from url query string: ", patientId);
     console.log("Auth url ", authURL);
-    fetchContextJson(authURL).then(json => {
-       if (patientId) {
-         // only do this IF patient id comes from url queryString
-         json.patientId = patientId;
-         sessionStorage.setItem(queryPatientIdKey, patientId);
-       }
-       // allow client id to be configurable
-       const envClientId = getEnv("REACT_APP_CLIENT_ID");
-       if (envClientId) json.clientId = envClientId;
+    fetchContextJson(authURL).then(
+      (json) => {
+        if (!json) {
+          setError("No valid context json specified");
+          return;
+        }
+        if (patientId) {
+          // only do this IF patient id comes from url queryString
+          json.patientId = patientId;
+          sessionStorage.setItem(queryPatientIdKey, patientId);
+        }
+        // allow client id to be configurable
+        const envClientId = getEnv("REACT_APP_CLIENT_ID");
+        if (envClientId) json.clientId = envClientId;
 
-       // allow auth scopes to be updated via environment variable
-       // see https://build.fhir.org/ig/HL7/smart-app-launch/scopes-and-launch-context.html
-       const envAuthScopes = getEnv("REACT_APP_AUTH_SCOPES");
-       if (envAuthScopes) json.scope = envAuthScopes;
-       
-       console.log("launch context json ", json);
-       FHIR.oauth2.authorize(json).catch((e) => {
-         setError(e);
-       });
-    });
+        // allow auth scopes to be updated via environment variable
+        // see https://build.fhir.org/ig/HL7/smart-app-launch/scopes-and-launch-context.html
+        const envAuthScopes = getEnv("REACT_APP_AUTH_SCOPES");
+        if (envAuthScopes) json.scope = envAuthScopes;
+
+        console.log("launch context json ", json);
+        FHIR.oauth2.authorize(json).catch((e) => {
+          setError(e);
+        });
+      },
+      (error) => setError(error)
+    );
   }, []);
 
   return (
