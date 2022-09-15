4e6f335ab6fc36ea1b491de369c8f7c23ee38be2
Amy Chen <amysbubble@Amys-MacBook-Pro-2.local>
2022-09-15 11:14:14 -0700

    add state var

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,26 @@
-import {Component} from "react";
+import {useEffect, useState} from "react";
 import FhirClientProvider from "../FhirClientProvider";
 import Summary from "./Summary";
 import "../style/App.scss";
 import {fetchEnvData, getEnvs} from "../util/util.js";
 
-class App extends Component {
-  componentWillMount() {
-    // calling function to get environment variables here to make sure they are accessible before component is rendered
+export default function App() {
+  const [appReady, setAppReady] = useState(false);
+  useEffect(() => {
     fetchEnvData();
+    setAppReady(true); // state change to make sure the environment variables are loaded
     console.log("environment variables ", getEnvs());
-  }
-  render() {
-    return (
-      <FhirClientProvider>
-        {<Summary></Summary>}
-        {/* add other components as needed */}
-      </FhirClientProvider>
-    );
-  }
+  }, []);
+  return (
+    <>
+      {
+        appReady &&
+        <FhirClientProvider>
+          {<Summary></Summary>}
+          {/* add other components as needed */}
+        </FhirClientProvider>
+      }
+    </>
+  );
 }
-export default App;
+
